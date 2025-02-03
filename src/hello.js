d3f623c3f57478d5de055decede9545856658a3f
heidless <lockhart.r@gmail.com>
2025-02-03 11:15:55 +0000

    init

---
+++ src/hello.js
@@ -0,0 +1,5 @@
+var greet = "Greetings";
+var geeks = "GeeksforGeeks";
+console.log(greet + " from " + geeks);
+// save the file as hello.ts 
+// tsc hello.ts | node hello.js
---
+++ src/hello.ts
@@ -0,0 +1,6 @@
+let greet: string = "Greetings"; 
+let geeks: string = "GeeksforGeeks"; 
+console.log(greet + " from " + geeks); 
+// save the file as hello.ts 
+
+// tsc hello.ts | node hello.js
